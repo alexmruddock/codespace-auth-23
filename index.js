ffc6ee2aedb5cb4beb1ef7c5ada2995bf8363ca3
alexmruddock <alex.ruddock@afinea.com>
2023-11-11 16:32:10 +0000

    added port

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const{PORT} = process.env
 
 app.listen(PORT, () => {
-    console.log('SERVER is running at port: ${PORT}')
-})
+    console.log(`SERVER is running at port: ${PORT}`);
+}) 
